622c92cb3ff74b31a916296d521705775bd36706
ET-SPARK <86984205+ET-SAPRK@users.noreply.github.com>
2023-07-31 22:52:51 +0300

    edited

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import About from './components/About'
+import Stars from './components/canvas/Stars'
+import Contact from './components/Contact'
+import Experience from './components/Experience'
+import Feedbacks from './components/Feedbacks'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import Tech from './components/Tech'
+import Works from './components/Works'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className='relative z-0'>
+          <Contact />
+          <Stars />
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
+import CanvasLoader from "../Loader";
 
 const Computers = () => {
+  const computer = useGLTF("./desktop_pc/scene.gltf");
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight
+        intensity={0.15}
+        groundColor="black" />
+    </mesh>
   )
 }
 

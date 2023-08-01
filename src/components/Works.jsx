b2692411b54dd184c1e3c0f4d831a6eeb909cb3c
ET-SPARK <86984205+ET-SAPRK@users.noreply.github.com>
2023-08-01 13:45:44 +0300

    edited

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { github } from "../assets";
+import { SectionWrapper } from "../hoc";
+import { projects } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const Works = () => {
   return (
-    <div>Works</div>
+    <>
+
+    </>
   )
 }
 
-export default Works
+export default StarWrapper(Works, "")
